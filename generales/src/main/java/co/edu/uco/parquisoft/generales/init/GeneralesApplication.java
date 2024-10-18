ce347ecfc9c53f7fcfea2f9c335f78a2f8d6c3d4
David-alzate <alzatedavid0126@gmail.com>
2024-10-18 09:39:23 -0500

    Creacion del proyecto con su microservicio generales

---
+++ generales/src/main/java/co/edu/uco/parquisoft/generales/init/GeneralesApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.parquisoft.generales.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralesApplication.class, args);
+	}
+
+}
---
+++ generales/src/test/java/co/edu/uco/parquisoft/generales/GeneralesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.parquisoft.generales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
