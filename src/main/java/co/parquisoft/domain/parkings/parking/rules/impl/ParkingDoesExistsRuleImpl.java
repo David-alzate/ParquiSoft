0843e3ea33b5c3185b3e6fa97fb6de2b56c3e860
David-alzate <alzatedavid0126@gmail.com>
2025-05-26 14:36:49 -0500

    Add Parking domain classes and exception handling for parking validation

---
+++ src/main/java/co/parquisoft/domain/parkings/parking/rules/impl/ParkingDoesExistsRuleImpl.java
@@ -0,0 +1,12 @@
+package co.parquisoft.domain.parkings.parking.rules.impl;
+
+import co.parquisoft.domain.parkings.parking.rules.ParkingDoesExistsRule;
+
+import java.util.UUID;
+
+public class ParkingDoesExistsRuleImpl implements ParkingDoesExistsRule {
+    @Override
+    public void validate(UUID data) {
+
+    }
+}
