8ef165713c7b01beae1835ce353db5d97f5d235c
David-alzate <alzatedavid0126@gmail.com>
2025-07-15 13:26:08 -0500

    Add ParkingSpotDoesExistsRule and implementation for validating parking spot existence

---
+++ src/main/java/co/parquisoft/domain/parkings/parkingspot/rule/ParkingSpotDoesExistsRule.java
@@ -0,0 +1,8 @@
+package co.parquisoft.domain.parkings.parkingspot.rule;
+
+import co.parquisoft.domain.DomainRule;
+
+import java.util.UUID;
+
+public interface ParkingSpotDoesExistsRule extends DomainRule<UUID> {
+}
