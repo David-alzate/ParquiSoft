66466015f54ada0ba13f7efd9e1f4664ac4c7527
David-alzate <alzatedavid0126@gmail.com>
2025-07-18 15:32:02 -0500

    Add UseCase and Interactor for getParkingSpot

---
+++ src/main/java/co/parquisoft/application/primaryports/interactor/parkings/parkingspot/GetParkingSpotInteractor.java
@@ -0,0 +1,9 @@
+package co.parquisoft.application.primaryports.interactor.parkings.parkingspot;
+
+import co.parquisoft.application.primaryports.dto.parkings.ParkingSpotDTO;
+import co.parquisoft.application.primaryports.interactor.InteractorWithoutInput;
+
+import java.util.List;
+
+public interface GetParkingSpotInteractor extends InteractorWithoutInput<List<ParkingSpotDTO>> {
+}
