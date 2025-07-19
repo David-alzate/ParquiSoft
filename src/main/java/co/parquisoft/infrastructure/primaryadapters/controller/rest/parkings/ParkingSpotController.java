69666f9f1aa38449581a6fb81f3cbb50668f052f
David-alzate <alzatedavid0126@gmail.com>
2025-07-19 11:24:00 -0500

    Add Controller for getParkingSpot

--- src/main/java/co/parquisoft/infrastructure/primaryadapters/controller/rest/parkings/ParkingSpotController.java
+++ src/main/java/co/parquisoft/infrastructure/primaryadapters/controller/rest/parkings/ParkingSpotController.java
@@ -1,15 +1,15 @@
 package co.parquisoft.infrastructure.primaryadapters.controller.rest.parkings;
 
 import co.parquisoft.application.primaryports.dto.parkings.ParkingSpotDTO;
+import co.parquisoft.application.primaryports.interactor.parkings.parkingspot.GetParkingSpotInteractor;
 import co.parquisoft.application.primaryports.interactor.parkings.parkingspot.RegisterNewParkingSpotInteractor;
 import co.parquisoft.crosscutting.exception.ParquiSoftException;
 import co.parquisoft.infrastructure.primaryadapters.controller.response.GenerateResponse;
 import co.parquisoft.infrastructure.primaryadapters.controller.response.GenericResponse;
+import co.parquisoft.infrastructure.primaryadapters.controller.response.parkings.ParkingSpotResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,9 +18,27 @@ import java.util.List;
 public class ParkingSpotController {
 
     private final RegisterNewParkingSpotInteractor registerNewParkingSpotInteractor;
+    private final GetParkingSpotInteractor getParkingSpotInteractor;
 
-    public ParkingSpotController(RegisterNewParkingSpotInteractor registerNewParkingSpotInteractor) {
+    public ParkingSpotController(RegisterNewParkingSpotInteractor registerNewParkingSpotInteractor, GetParkingSpotInteractor getParkingSpotInteractor) {
         this.registerNewParkingSpotInteractor = registerNewParkingSpotInteractor;
+        this.getParkingSpotInteractor = getParkingSpotInteractor;
+    }
+
+    @GetMapping
+    public ResponseEntity<ParkingSpotResponse> getParkings() {
+        try {
+            List<ParkingSpotDTO> data = getParkingSpotInteractor.execute();
+            var response = ParkingSpotResponse.build(List.of("Celdas consultadas exitosamente"), data);
+            return GenerateResponse.generateSuccessResponseWithData(response);
+        } catch (final ParquiSoftException exception) {
+            var response = ParkingSpotResponse.build(List.of(exception.getUserMessage()), List.of());
+            return GenerateResponse.generateBadRequestResponseWithData(response);
+        } catch (final Exception exception) {
+            var userMessage = "Se ha presentado un problema tratando de consultar las celdas";
+            var response = ParkingSpotResponse.build(List.of(userMessage), List.of());
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping
