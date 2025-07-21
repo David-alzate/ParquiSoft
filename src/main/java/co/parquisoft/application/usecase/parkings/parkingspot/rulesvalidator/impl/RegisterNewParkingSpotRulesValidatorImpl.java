ee40bd6b878a754f514097c59b067a4314a9b996
David-alzate <alzatedavid0126@gmail.com>
2025-07-21 10:55:19 -0500

    Add VehicleType validation to RegisterNewParkingSpotRulesValidator

--- src/main/java/co/parquisoft/application/usecase/parkings/parkingspot/rulesvalidator/impl/RegisterNewParkingSpotRulesValidatorImpl.java
+++ src/main/java/co/parquisoft/application/usecase/parkings/parkingspot/rulesvalidator/impl/RegisterNewParkingSpotRulesValidatorImpl.java
@@ -3,19 +3,23 @@ package co.parquisoft.application.usecase.parkings.parkingspot.rulesvalidator.im
 import co.parquisoft.application.usecase.parkings.parkingspot.rulesvalidator.RegisterNewParkingSpotRulesValidator;
 import co.parquisoft.domain.parkings.branch.rules.BranchDoesExistsRule;
 import co.parquisoft.domain.parkings.parkingspot.ParkingSpotDomain;
+import co.parquisoft.domain.vehicle.rules.VehicleTypeDoesExitsRule;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegisterNewParkingSpotRulesValidatorImpl implements RegisterNewParkingSpotRulesValidator {
 
     private final BranchDoesExistsRule branchDoesExistsRule;
+    private final VehicleTypeDoesExitsRule vehicleTypeDoesExitsRule;
 
-    public RegisterNewParkingSpotRulesValidatorImpl(BranchDoesExistsRule branchDoesExistsRule) {
+    public RegisterNewParkingSpotRulesValidatorImpl(BranchDoesExistsRule branchDoesExistsRule, VehicleTypeDoesExitsRule vehicleTypeDoesExitsRule) {
         this.branchDoesExistsRule = branchDoesExistsRule;
+        this.vehicleTypeDoesExitsRule = vehicleTypeDoesExitsRule;
     }
 
     @Override
     public void validate(ParkingSpotDomain data) {
         branchDoesExistsRule.validate(data.getBranch().getId());
+        vehicleTypeDoesExitsRule.validate(data.getVehicleType().getId());
     }
 }
