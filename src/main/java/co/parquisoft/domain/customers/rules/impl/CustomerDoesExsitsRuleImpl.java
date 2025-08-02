c394dc9c0e3bbcfbad8a9289bd5a128d611d106b
David-alzate <alzatedavid0126@gmail.com>
2025-08-02 14:32:40 -0500

    Implement customer existence validation rules in CustomerDoesExistsRuleImpl and CustomerDoesNotExistsRuleImpl

--- src/main/java/co/parquisoft/domain/customers/rules/impl/CustomerDoesExsitsRuleImpl.java
+++ src/main/java/co/parquisoft/domain/customers/rules/impl/CustomerDoesExsitsRuleImpl.java
@@ -1,5 +1,7 @@
 package co.parquisoft.domain.customers.rules.impl;
 
+import co.parquisoft.application.secondaryports.repository.customers.CustomerRepository;
+import co.parquisoft.domain.customers.exceptions.CustomerDoesNotExsitsException;
 import co.parquisoft.domain.customers.rules.CustomerDoesExsitsRule;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +10,16 @@ import java.util.UUID;
 @Service
 public class CustomerDoesExsitsRuleImpl implements CustomerDoesExsitsRule {
 
+    private final CustomerRepository customerRepository;
+
+    public CustomerDoesExsitsRuleImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     @Override
     public void validate(UUID data) {
-
+        if (!customerRepository.existsById(data)) {
+            throw CustomerDoesNotExsitsException.create();
+        }
     }
 }
--- src/main/java/co/parquisoft/domain/customers/rules/impl/CustomerDoesNotExistsRuleImpl.java
+++ src/main/java/co/parquisoft/domain/customers/rules/impl/CustomerDoesNotExistsRuleImpl.java
@@ -1,5 +1,7 @@
 package co.parquisoft.domain.customers.rules.impl;
 
+import co.parquisoft.application.secondaryports.repository.customers.CustomerRepository;
+import co.parquisoft.domain.customers.exceptions.CustomerDoesExistsException;
 import co.parquisoft.domain.customers.rules.CustomerDoesNotExistsRule;
 import org.springframework.stereotype.Service;
 
@@ -8,8 +10,16 @@ import java.util.UUID;
 @Service
 public class CustomerDoesNotExistsRuleImpl implements CustomerDoesNotExistsRule {
 
+    private final CustomerRepository customerRepository;
+
+    public CustomerDoesNotExistsRuleImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     @Override
     public void validate(UUID data) {
-
+        if (customerRepository.existsById(data)) {
+            throw CustomerDoesExistsException.create();
+        }
     }
 }
